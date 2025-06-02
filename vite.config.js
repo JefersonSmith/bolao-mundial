de5262fa43e5c7b97c148113f36975742fa51e3e
Jeferson Smith <jjsmithcrf@gmail.com>
2025-06-02 13:32:05 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione a opção 'base' aqui para o deploy no GitHub Pages
+  // Substitua 'bolao-mundial-react' pelo nome EXATO do seu repositório no GitHub
+  base: '/bolao-mundial-react/',
   plugins: [react()],
 })
