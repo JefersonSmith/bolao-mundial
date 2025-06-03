07b9738bef58bc65d83fd380133493a48cd5c723
Jeferson Smith <jjsmithcrf@gmail.com>
2025-06-02 22:10:25 -0300

    update logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ import flamengoLogo from '../assets/Fla.png'
 function Header({ title, subtitle }) {
   return (
     <header className="app-header">
-      <img src={flamengoLogo} alt="Escudo do Flamengo" className="flamengo-logo" />
       <div>
         <h1>{title}</h1>
         <p>{subtitle}</p>
